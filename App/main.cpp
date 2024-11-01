e0bb24a339afa11c5a4105d9e563e9ed45babfce
Yuri Solyanik <jura.s.n@gmail.com>
2024-11-01 13:30:23 +0500

    Добавил вывод русских букв в консоль

--- App/main.cpp
+++ App/main.cpp
@@ -1,7 +1,12 @@
-﻿#include <iostream>
+﻿#include <Windows.h>
+#include <iostream>
 
 int main() {
-	std::cout << "Hello CMake." << std::endl;
+//	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Проверка русского языка в консоли." << std::endl;
 
 	return 0;
 }
